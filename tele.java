9e5103802596fb85aed6bbbe15fb2bf8d59153b0
Davidarduinoclone <55816432+Davidarduinoclone@users.noreply.github.com>
2021-12-03 10:42:33 -0500

    Add files via upload

---
+++ tele.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.teamcode.Drivetrain;
+import org.firstinspires.ftc.teamcode.Subsystem;
+
+@TeleOp(name="tele", group="Linear Opmode")
+
+public class tele extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+  
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        Subsystem.initialize(hardwareMap, telemetry);
+        Drivetrain.initialize();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+           
+            leftPower    =  1.0;
+            rightPower   =  1.0;
+            //Caleb servoPower   =  0.1;
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            double y = -gamepad1.left_stick_y/2;
+            double x = (gamepad1.left_stick_x * 1.1)/2 ;
+            double rx  = gamepad1.right_stick_x/2;
+            double crawmove = gamepad1.right_stick_y;
+            // Changed t to right stick button
+            Drivetrain.move(y,x,rx);
+            
+            //Drivetrain.SetPosition(10000, 0.0, 0.0);
+            
+           
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            //telemetry.addData("Motors", "left (%.2f), right (%.2f) (%.2f)", leftPower, rightPower, c);
+            //telemetry.addData("Status (%.2f)" + rightturn);
+            telemetry.update();
+        }
+    }
+}
